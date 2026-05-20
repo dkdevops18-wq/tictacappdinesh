f86a8ca4fb2759284cb90feaffc6911c7151fc09
dkdevops18-wq <dkdevops18@gmail.com>
2026-05-20 19:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ def echo():
 
 if __name__ == '__main__':
     # 4. Run the development server
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
